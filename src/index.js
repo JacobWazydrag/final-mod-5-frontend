9e74c563e61a21b3e664bfd667c38814e8f5de5c
J-Waz <jgw.jakegeorge@gmail.com>
2020-09-17 09:01:01 -0500

    a possible fix to redux state

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import rootReducer from './redux/rootReducer';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import rootReducer from "./redux/rootReducer";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./App";
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage () {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined 
+    return JSON.parse(serializedState)
+  } catch(e) {
+    console.log(e)
+    return undefined
+  }
+}
+
+const persistedState = loadFromLocalStorage()
 
 const store = createStore(
-  rootReducer, 
+  rootReducer,
+  persistedState,
   compose(
     applyMiddleware(thunk),
-    window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
   )
 );
 
+store.subscribe(() => {
+  saveToLocalStorage(store.getState())
+})
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
