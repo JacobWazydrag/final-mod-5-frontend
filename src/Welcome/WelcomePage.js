8e66c956f34cd4bce14ed21026a6f42073b1ca90
J-Waz <jgw.jakegeorge@gmail.com>
2020-09-17 07:04:29 -0500

    small bugs fixed

--- src/Welcome/WelcomePage.js
+++ src/Welcome/WelcomePage.js
@@ -9,15 +9,15 @@ const WelcomePage = () => {
     <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
     <h1>This is the Welcome WelcomePage.js</h1>
 
-      <div class="row">
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
+      <div className="row">
+    <div className="col s12 m6">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Card Title</span>
           <p>I am a very simple card. I am good at containing small bits of information.
           I am convenient because I require little markup to use effectively.</p>
         </div>
-        <div class="card-action">
+        <div className="card-action">
         <Link to="/student_signup">!! Students !!</Link>
         </div>
       </div>
